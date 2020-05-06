ca7df993b5b57271d89ec483bae2904dc8ba0b16
Boris Bonev <bonevbs@gmail.com>
2020-05-06 13:14:16 +0200

    Minor changes in clean_hss.m.

--- @hss/clean_hss.m
+++ @hss/clean_hss.m
@@ -1,9 +1,9 @@
 function H = clean_hss(H)
   H.topnode = 1;
-  H = clean_ric(H);
+  H = clean_rec(H);
 end
 
-function H = clean_ric(H)
+function H = clean_rec(H)
   if isempty(H.A11) && isempty(H.A22)
     H.leafnode = 1;
     H.Wl = []; H.Wr = []; H.Rl = []; H.Rr = [];
@@ -12,10 +12,10 @@ function H = clean_ric(H)
     H.leafnode = 0;
     H.U = []; H.V = [];
     if ~isempty(H.A11)
-      H.A11 = clean_ric(H.A11);
+      H.A11 = clean_rec(H.A11);
     end
     if ~isempty(H.A22)
-      H.A22 = clean_ric(H.A22);
+      H.A22 = clean_rec(H.A22);
     end
   end
 end
